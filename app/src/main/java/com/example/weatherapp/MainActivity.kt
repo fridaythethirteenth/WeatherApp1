3d9d824ef707736936ee3a9688840684aacd0eba
Nate <47154086+fridaythethirteenth@users.noreply.github.com>
2022-10-11 21:52:12 -0500

    Compose Part 1
    Implemented compose function for CurrentConditionsScreen

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.weatherapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.weatherapp.ui.CurrentConditions
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContent {
+            val navController = rememberNavController()
+            NavHost(navController, startDestination = "CurrentConditions" ) {
+                composable("CurrentConditions") {
+                    CurrentConditions()
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/weatherapp/ui/CurrentCondtionsScreen.kt
@@ -0,0 +1,86 @@
+package com.example.weatherapp.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.Modifier
+import com.example.weatherapp.R
+
+
+
+@Composable
+fun CurrentConditions() {
+    Column(
+            modifier = Modifier.padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+
+    ) {
+        Text(
+            text = stringResource(id = R.string.city_name),
+            style = TextStyle(
+                fontWeight = FontWeight(600),
+                fontSize = 24.sp
+            )
+        )
+        Row(
+            modifier = Modifier.padding(horizontal = 16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = stringResource(id = R.string.current_temp),
+                    style = TextStyle(
+                        fontWeight = FontWeight(400),
+                        fontSize = 72.sp
+                    )
+                )
+                Text(
+                    text = stringResource(id = R.string.feels_like),
+                    style = TextStyle(
+                        fontWeight = FontWeight(400),
+                        fontSize = 12.sp
+                    )
+                )
+            }
+            Spacer(modifier = Modifier.weight(1f, fill = true))
+            Image(
+                modifier = Modifier.size(72.dp),
+                painter = painterResource(R.drawable.sunny),
+                contentDescription = "Sunny")
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+        ) {
+            val textStyle = TextStyle(
+                fontSize = 16.sp
+            )
+            Text(text = stringResource(id = R.string.high_temp), style = textStyle)
+            Text(text = stringResource(id = R.string.low_temp), style = textStyle)
+            Text(text = stringResource(id = R.string.humidity), style = textStyle)
+            Text(text = stringResource(id = R.string.pressure), style = textStyle)
+        }
+
+    }
+
+}
+
+@Preview(
+    showSystemUi = true
+)
+@Composable
+fun CurrentConditionsPreview() {
+    CurrentConditions()
+}
